1193aca02b9e0dde72b114c7415e269b1480a194
minju <minju9711@naver.com>
2025-03-10 22:25:24 +0900

    [modify] 레디스 메시지 브로커로 변경 [add] SockJs 사용할 것임을 명시

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,21 +8,21 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    //스프링의 인메모리 사용하는 코드
+    //레디스 메시지 브로커 사용
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/sub");
-        config.setApplicationDestinationPrefixes("/pub");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub"); // 구독 (Subscribe)
+        registry.setApplicationDestinationPrefixes("/pub"); // 발행 (Publish)
     }
 
     //초기 핸드쉐이크 과정에서 사용할 endpoint
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-connect") //나중에 https 통신할거면 wws로 수정하면 됨.
-                .setAllowedOrigins("*");
+        registry.addEndpoint("/ws-connect") //https 사용시 wws 하면 됨
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
-
 }
+
