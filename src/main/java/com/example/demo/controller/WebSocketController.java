9e687644f2f0382773828f9179c69d156ae0ebe3
minju <minju9711@naver.com>
2025-03-18 23:01:56 +0900

    [modify] 채널명 하드 코딩 대신 constants로 관리하도록 로직 변경

--- src/main/java/com/example/demo/controller/WebSocketController.java
+++ src/main/java/com/example/demo/controller/WebSocketController.java
@@ -8,6 +8,8 @@ import org.springframework.messaging.handler.annotation.*;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 
+import static com.example.demo.config.WebSocketConstants.*;
+
 
 //WebSocket으로 들어온 메시지를 Redis에 발행
 @Controller
@@ -18,40 +20,46 @@ public class WebSocketController {
     // 1. 전체 채팅방 메시지 전송
     @MessageMapping("/chat/all")
     public void sendMessageToAll(@Payload ChatMessageReq message) {
-        redisTemplate.convertAndSend("chat:all", message);
+        redisTemplate.convertAndSend(CHAT_ALL_CHANNEL, message);
     }
 
     // 2. 귓속말 전송
     @MessageMapping("/chat/private")
     public void sendPrivateMessage(@Payload ChatMessageReq message) {
-        String targetChannel = "chat:private:" + message.nickName();
+        String targetChannel = CHAT_PRIVATE_CHANNEL + message.nickName();
         redisTemplate.convertAndSend(targetChannel, message);
     }
 
     // 3. 대기방 메시지 전송
     @MessageMapping("/chat/room/{roomId}")
     public void sendRoomMessage(@DestinationVariable String roomId, @Payload ChatMessageReq message) {
-        redisTemplate.convertAndSend("chat:room:" + roomId, message);
+        redisTemplate.convertAndSend(CHAT_ROOM_CHANNEL + roomId, message);
     }
 
     // 4. 대기방 레디 상태 업데이트
     @MessageMapping("/game/ready/{roomId}")
     public void sendReadyStatus(@DestinationVariable String roomId, @Payload GameReadyStatus readyStatus) {
-        redisTemplate.convertAndSend("game:ready:" + roomId, readyStatus);
+        redisTemplate.convertAndSend(GAME_READY_CHANNEL + roomId, readyStatus);
     }
 
-    // 5. 게임 시작
+    //5. 게임 시작 전 게임 정보(제시어) 전달
+    @MessageMapping("/game/info/{roomId}")
+    public void sendGameInfoBeforeStart(@DestinationVariable String roomId) {
+
+    }
+
+    // 6. 게임 시작
     @MessageMapping("/game/start/{roomId}")
     public void startGame(@DestinationVariable String roomId) {
         long endTime = System.currentTimeMillis() + 30000; // 30초 후 종료
         GameEventReq startMessage = new GameEventReq(roomId, endTime);
-        redisTemplate.convertAndSend("game:start:" + roomId, startMessage);
+        redisTemplate.convertAndSend(GAME_START_CHANNEL + roomId, startMessage);
     }
 
-    // 6. 게임 종료
+    // 7. 게임 종료
     @MessageMapping("/game/end/{roomId}")
     public void endGame(@DestinationVariable String roomId) {
-        redisTemplate.convertAndSend("game:end:" + roomId, "게임 종료");
+        redisTemplate.convertAndSend(GAME_END_CHANNEL + roomId, GAME_END_MESSAGE);
     }
 
 }
